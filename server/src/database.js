7219dbdeaf95d0b3e94f4d4ccbdb1492b9340b9c
adisve <adis.veletanlic@gmail.com>
2023-04-10 12:04:00 +0200

    Add all routes for mongodb

---
+++ server/src/database.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+const uri = process.env.CONNECTION_URL || '';
+const client = new MongoClient(uri);
+let _db;
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB Atlas');
+    _db = client.db('albums');
+  } catch (error) {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  }
+}
+
+const getDb = () => {
+  return _db;
+};
+
+module.exports = {
+  getDb,
+  connectToDatabase,
+};
