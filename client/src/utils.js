6d8970ec2d2cac6f1aa6f6e74362a0a41678c890
adisve <adis.veletanlic@gmail.com>
2023-04-20 13:49:02 +0200

    Add web_service.js, acting as network repo

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,7 +1,13 @@
+import {
+  getAllAlbums,
+  deleteAlbum,
+  updateAlbum,
+  createAlbum,
+} from './web_service.js';
+
 /**
  * @fileoverview Utility functions for the client side.
  */
-const url = 'http://localhost:8080/api';
 
 /**
  * Create main html page header and
@@ -10,7 +16,7 @@ const url = 'http://localhost:8080/api';
 export function createHeader() {
   const root = document.querySelector('#root');
   const header = document.createElement('h1');
-  header.innerHTML = 'Albums';
+  header.textContent = 'Albums';
   header.id = 'header';
   root.appendChild(header);
 }
@@ -18,68 +24,49 @@ export function createHeader() {
 /**
  * Create a table with all albums retrieved from MongoDB
  */
-export function createTable() {
+export async function createTable() {
   const root = document.querySelector('#root');
-  getAllAlbums().then((albums) => {
-    const tableHead = `
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th class='artist-header' style='display: none;'>Artist</th> 
-          <th class='year-header' style='display: none;'>Year</th>
-          <th class='genre-header' style='display: none;'>Genre</th>
-          <th class='track-header' style='display: none;'>Tracks</th>
-          <th>Edit</th>
-        </tr>
-      </thead>
+  const albums = await getAllAlbums();
+  const tableHead = `
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th class='artist-header' style='display: none;'>Artist</th> 
+        <th class='year-header' style='display: none;'>Year</th>
+        <th class='genre-header' style='display: none;'>Genre</th>
+        <th class='track-header' style='display: none;'>Tracks</th>
+        <th>Edit</th>
+      </tr>
+    </thead>
+  `;
+  const tableRows = Array.from(albums, (album, index) => {
+    return `
+      <tr id='album-${index}' data-album-id='${album._id}'>
+        <td contentEditable>${album.title}</td>
+        <td class='artist' style='display: none;' contentEditable>${album.artist}</td>
+        <td class='year' style='display: none;' contentEditable>${album.year}</td>
+        <td class='genre' style='display: none;' contentEditable>${album.genre}</td>
+        <td class='tracks' style='display: none;' contentEditable>${album.tracks}</td>
+        <td style="line-height: 50px; text-align: center;">
+          <button class='button show-details-button' id='show-details-${index}'>Show details</button>
+          <button class='button-update' id='update-${index}'>Update</button>
+          <button class='button-delete' id='delete-${index}'>Delete</button>
+        </td>
+      </tr>
     `;
-    const tableRows = albums.map((album, index) => {
-      return `
-        <tr id='album-${index}' data-album-id='${album._id}'>
-          <td contentEditable>${album.title}</td>
-          <td class='artist' style='display: none;' contentEditable>${
-            album.artist
-          }</td>
-          <td class='year' style='display: none;' contentEditable>${
-            album.year
-          }</td>
-          <td class='genre' style='display: none;' contentEditable>${
-            album.genre
-          }</td>
-          <td class='tracks' style='display: none;' contentEditable>${album.tracks.join(
-            ', '
-          )}</td>
-          <td style="line-height: 50px; text-align: center;">
-            <button class='button show-details-button' id='show-details-${index}'>Show details</button>
-            <button class='button-update' id='update-${index}'>Update</button>
-            <button class='button-delete' id='delete-${index}'>Delete</button>
-          </td>
-        </tr>
-      `;
-    });
-    const content = `
-      <table id='album-table' class="styled-table">
-        ${tableHead}
-        <tbody>
-          ${tableRows.join('')}
-        </tbody>
-      </table>
-    `;
-    const table = document.createElement('div');
-    table.innerHTML = content;
-    root.appendChild(table);
-    addButtonListeners();
   });
-}
-
-/**
- * Retrieves all albums from MongoDB
- * @returns Array of all albums
- */
-export async function getAllAlbums() {
-  let response = await fetch(`${url}/albums/`);
-  let albums = await response.json();
-  return albums;
+  const content = `
+    <table id='album-table' class="styled-table">
+      ${tableHead}
+      <tbody>
+        ${tableRows.join('')}
+      </tbody>
+    </table>
+  `;
+  const table = document.createElement('div');
+  table.innerHTML = content;
+  root.appendChild(table);
+  addButtonListeners();
 }
 
 /**
@@ -91,108 +78,57 @@ export async function createNewAlbumTable() {
   const createAlbumTable = document.createElement('div');
   createAlbumTable.id = 'create-album-table';
   const content = `
-        <table class="styled-table">
-            <thead>
-                <tr>
-                  <th>Title</th>
-                  <th>Artist</th> 
-                  <th>Year</th>
-                  <th>Genre</th>
-                  <th>Tracks</th>
-                  <th>Create</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td contentEditable></td>
-                    <td contentEditable></td>
-                    <td contentEditable></td>
-                    <td contentEditable></td>
-                    <td contentEditable></td>
-                    <td style="line-height: 50px; text-align: center;">
-                      <button class='button' id='create-album'>Create album</button>
-                  </td>
-                </tr>
-            </tbody>
-        </table>
-    `;
+    <table class="styled-table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Artist</th> 
+          <th>Year</th>
+          <th>Genre</th>
+          <th>Tracks</th>
+          <th>Create</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td contentEditable></td>
+          <td contentEditable></td>
+          <td contentEditable></td>
+          <td contentEditable></td>
+          <td contentEditable></td>
+          <td style="line-height: 50px; text-align: center;">
+            <button class='button' id='create-album'>Create album</button>
+          </td>
+        </tr>
+      </tbody>
+      `;
   createAlbumTable.innerHTML = content;
   root.appendChild(createAlbumTable);
   addCreateAlbumButtonListener();
 }
 
 /**
- * Creates an album based on object data
- * @param {string} title
- * @param {string} artist
- * @param {string} year
- * @param {string} genre
- * @param {Array} tracks
- */
-async function createAlbum(title, artist, year, genre, tracks) {
-  await fetch(`${url}/albums/`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ title, artist, year, genre, tracks }),
-  });
-  clearCreateAlbumTable();
-  createTable();
-}
 
-/**
- * Deletes an album based on id
- * @param {string} id
- */
-async function deleteAlbum(id) {
-  const tableRow = document.querySelector(`#album-${id}`);
-  const albumId = tableRow.dataset.albumId;
-  await fetch(`${url}/albums/${albumId}`, {
-    method: 'DELETE',
-  });
-  document.querySelector('#album-table').remove();
-  createTable();
-}
-
-/**
- * Updates an album based on id
- * @param {int} index
- */
-export async function updateAlbum(index) {
-  const tableRow = document.querySelector(`#album-${index}`);
-  const id = tableRow.dataset.albumId;
-  const title = tableRow.querySelector('td:nth-child(1)').textContent.trim();
-  const artist = tableRow.querySelector('td:nth-child(2)').textContent.trim();
-  const year = tableRow.querySelector('td:nth-child(3)').textContent.trim();
-  const genre = tableRow.querySelector('td:nth-child(4)').textContent.trim();
-  const tracks = tableRow.querySelector('td:nth-child(5)').textContent.trim();
-
-  await fetch(`${url}/albums/${id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ title, artist, year, genre, tracks }),
-  });
-}
-
-/**
- * Adds event listeners to all buttons in album-table
- */
+Adds event listeners to all buttons in album-table
+*/
 function addButtonListeners() {
   const updateButtons = document.querySelectorAll('.button-update');
   const deleteButtons = document.querySelectorAll('.button-delete');
   const showDetailsButtons = document.querySelectorAll('.show-details-button');
   deleteButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
-      console.log(`Pressed delete button for album ${index}`);
-      deleteAlbum(index);
+      console.log('Pressed delete button for album ${index}');
+      deleteAlbum(index, () => {
+        // Delete the album-table and create a new one
+        const albumTable = document.querySelector('#album-table');
+        albumTable.parentNode.removeChild(albumTable);
+        createTable();
+      });
     });
   });
   updateButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
-      console.log(`Pressed update button for album ${index}`);
+      console.log('Pressed update button for album ${index}');
       updateAlbum(index);
     });
   });
@@ -231,36 +167,31 @@ function addButtonListeners() {
     });
   });
 }
-
 /**
- * Adds event listener to 'Create album' button
- */
+
+Adds event listener to 'Create album' button
+*/
 function addCreateAlbumButtonListener() {
   const createAlbumButton = document.querySelector('#create-album');
-  createAlbumButton.addEventListener('click', () => {
+  createAlbumButton.addEventListener('click', async () => {
     console.log('Pressed create album button');
-    let title = document
-      .querySelector('#create-album-table td:nth-child(1)')
-      .textContent.trim();
-    let artist = document
-      .querySelector('#create-album-table td:nth-child(2)')
-      .textContent.trim();
-    let year = document
-      .querySelector('#create-album-table td:nth-child(3)')
-      .textContent.trim();
-    let genre = document
-      .querySelector('#create-album-table td:nth-child(4)')
-      .textContent.trim();
-    let tracks = document
-      .querySelector('#create-album-table td:nth-child(5)')
-      .textContent.trim();
-    createAlbum(title, artist, year, genre, tracks.split());
+    const [title, artist, year, genre, tracks] = Array.from(
+      document.querySelectorAll('#create-album-table td'),
+      (td) => td.textContent.trim()
+    );
+    await createAlbum(title, artist, year, genre, tracks.split(', '), () => {
+      clearCreateAlbumTable();
+      // Delete the album-table and create a new one
+      const albumTable = document.querySelector('#album-table');
+      albumTable.parentNode.removeChild(albumTable);
+      createTable();
+    });
   });
 }
-
 /**
- * Clears the table for creating a new album
- */
+
+Clears the table for creating a new album
+*/
 function clearCreateAlbumTable() {
   document.querySelector('#create-album-table td:nth-child(1)').textContent =
     '';
